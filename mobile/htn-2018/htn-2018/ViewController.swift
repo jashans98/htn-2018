9955ed125ada060e96fbb0823832b648f652dbc1
Jashan Shewakramani <jashan1998s@gmail.com>
2018-09-15 00:12:21 -0400

    Create xcode project

---
+++ mobile/htn-2018/htn-2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  htn-2018
+//
+//  Created by Jashan Shewakramani on 2018-09-15.
+//  Copyright © 2018 Jashan Shewakramani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
