dcce34637b9961cd4031b1a63a24c8bf7dafc59a
raphael <raph.koh@gmail.com>
2018-09-15 21:28:54 -0400

    Add myscript

---
+++ server/core/myscript.js
@@ -0,0 +1,39 @@
+const url = "https://cloud.myscript.com/api/v4.0/iink/batch";
+const request = require('request');
+
+const options = {
+  url,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json, application/x-latex, text/plain, application/vnd.myscript.jiix',
+    'applicationKey': 'd44cfc60-68b8-49ae-8a46-fb0a234b431b',
+    'hmac': 'bcb5cd96-9904-4bb5-bc97-e11c1477f243',
+  },
+};
+
+function translate({
+  dpi,
+  width,
+  height,
+  strokes,
+}) {
+  const form = {
+    xDpi: dpi,
+    yDpi: dpi,
+    width,
+    height,
+    contentType: 'Math',
+    strokeGroups: [{
+      strokes
+    }]
+  };
+  const ops = Object.assign({}, options);
+  ops.form = form;
+  return new Promise(resolve => {
+    request.post(ops, (err, resp, body) => resolve(body));
+  });
+}
+
+module.exports = {
+  translate,
+};
