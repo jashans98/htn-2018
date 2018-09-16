ef68c36e2de30cce57bcc32bb18e3490e7282c3c
Jashan Shewakramani <jashan1998s@gmail.com>
2018-09-15 20:08:37 -0400

    Canvas/Stroke Inputs + Rendering

---
+++ mobile/htn-2018/htn-2018/EditDocumentToolbarView.swift
@@ -0,0 +1,75 @@
+//
+//  EditDocumentToolbarView.swift
+//  htn-2018
+//
+//  Created by Jashan Shewakramani on 2018-09-15.
+//  Copyright © 2018 Jashan Shewakramani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+protocol EditDocumentToolbarViewDelegate: class {
+    func editDocumentToolBarViewDidTapNewMathSection(_ view: EditDocumentToolbarView)
+    func editDocumentToolBarViewDidTapNewTextSection(_ view: EditDocumentToolbarView)
+}
+
+class EditDocumentToolbarView: UIView {
+    let newMathButton = UIButton()
+    let newTextButton = UIButton()
+    
+    weak var delegate: EditDocumentToolbarViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+        self.addSubview(self.newMathButton)
+        self.addSubview(self.newTextButton)
+        
+        self.newMathButton.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(Dimens.screenPadding)
+        }
+        
+        self.newMathButton.setTitle("New Math Box", for: .normal)
+        self.newMathButton.setTitleColor(Colors.gray1, for: .normal)
+        
+        self.newTextButton.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.right.equalToSuperview().inset(Dimens.screenPadding)
+        }
+        
+        self.newTextButton.setTitle("New Text Box", for: .normal)
+        self.newTextButton.setTitleColor(Colors.gray1, for: .normal)
+        
+        self.newMathButton.addTarget(self, action: #selector(didTapMath), for: .touchUpInside)
+        self.newTextButton.addTarget(self, action: #selector(didTapText), for: .touchUpInside)
+    }
+    
+    @objc private func didTapMath() {
+        self.delegate?.editDocumentToolBarViewDidTapNewMathSection(self)
+    }
+    
+    @objc private func didTapText() {
+        self.delegate?.editDocumentToolBarViewDidTapNewTextSection(self)
+    }
+}
+
+
+
+
+
+
+
+
+
+
